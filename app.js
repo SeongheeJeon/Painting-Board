fe29f29a68a6195ee4897c8be990d4a303331293
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-02-14 20:38:42 +0900

    Change mouse event function
    Do  beginPath() in onMouseUp and remove moveTo().
    
    If mouse moves without click, path doesn't begin or move.

--- app.js
+++ app.js
@@ -20,26 +20,22 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-  } else {
+
+  if (painting) {
     ctx.lineTo(x, y);
     ctx.stroke();
+    console.log(ctx);
   }
 }
 
 function onMouseDown(event) {
   painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
+  ctx.beginPath();
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
