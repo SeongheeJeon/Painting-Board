07fc8e4ee0c9ba12738cba0a5aa88599d170a626
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-02-14 21:25:12 +0900

    Add color changing event

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 400;
 canvas.height = 400;
@@ -24,7 +25,6 @@ function onMouseMove(event) {
   if (painting) {
     ctx.lineTo(x, y);
     ctx.stroke();
-    console.log(ctx);
   }
 }
 
@@ -33,9 +33,19 @@ function onMouseDown(event) {
   ctx.beginPath();
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
